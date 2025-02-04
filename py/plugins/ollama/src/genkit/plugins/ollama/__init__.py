8d4c6d75e62febf7c8fab9cab72e8c913c224ccc
Kyrylo Hrymailo <kirill.grim@gmail.com>
2025-02-04 18:54:37 +0100

    [py] Initial implementation of OLLAMA plugin

--- py/plugins/ollama/src/genkit/plugins/ollama/__init__.py
+++ py/plugins/ollama/src/genkit/plugins/ollama/__init__.py
@@ -1,14 +1,14 @@
 # Copyright 2025 Google LLC
 # SPDX-License-Identifier: Apache-2.0
 
-
-"""
-Ollama Plugin for Genkit.
-"""
+from genkit.plugins.ollama.plugin_api import Ollama
 
 
 def package_name() -> str:
     return 'genkit.plugins.ollama'
 
 
-__all__ = ['package_name']
+__all__ = [
+    package_name.__name__,
+    Ollama.__name__,
+]
